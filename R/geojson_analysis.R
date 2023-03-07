f4a089d4b2e4cb058953671b4589f16ec8e59a56
EAMENA-M <115703632+EAMENA-M@users.noreply.github.com>
2023-03-07 14:21:54 +0000

    Update geojson_analysis.R

--- R/geojson_analysis.R
+++ R/geojson_analysis.R
@@ -1,18 +1,18 @@
-#' Complex interactive statistics on a GeoJSON file
+#' Complex interactive statistics on GeoJSON file
 #'
 #' @name geojson_analysis
 #'
-#' @description Complex interactive statistics on a GeoJSON file with the Shiny package
+#' @description Complex interactive statistics on GeoJSON file with Shiny package
 #'
-#' @param stat.name the name of the output file. By default "stat".
-#' @param geojson.path the path of the GeoJSON file. By default 'caravanserail.geojson'.
-#' @param ids the IDs of the resources, by default "EAMENA.ID" (nb: R field names format, without spaces).
-#' @param stat the statistic that will be computed. By default 'list_fields' (list the fields). The other options are: "list_ids" list EAMENA IDs ; etc.
-#' @param export.stat if TRUE return the stats to be stored in a new variable.
-#' @param write.stat if TRUE, export the stats in a new file, if FALSE will only display it.
-#' @param dirOut the folder where the outputs will be saved. By default: '/results'. If it doesn't exist, it will be created. Only useful is write.stat is TRUE.
+#' @param stat.name name of output file. Default "stat".
+#' @param geojson.path path of the GeoJSON file. Default 'caravanserail.geojson'.
+#' @param ids IDs of resources, by default "EAMENA.ID" (nb: R field name format, without spaces).
+#' @param stat statistic to be computed. By default 'list_fields' (list the fields). Alt options: "list_ids" list EAMENA IDs ; etc.
+#' @param export.stat if TRUE return stats to be stored in new variable.
+#' @param write.stat if TRUE, export stats in new file, if FALSE will only display it.
+#' @param dirOut folder where outputs will be saved. By default: '/results'. If doesn't exist, it will be created. Only useful if write.stat is TRUE.
 #'
-#' @return Show or export basic statistics on the GeoJSON file
+#' @return Show or export basic statistics on GeoJSON file
 #'
 #' @examples
 #'
